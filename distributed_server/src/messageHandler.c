151dde57b2de32c58b7f0cbbdff6e7bf48f3f4aa
Leonardo Barreiros <leossb36@gmail.com>
2021-04-19 15:15:22 -0300

    .

--- distributed_server/src/messageHandler.c
+++ distributed_server/src/messageHandler.c
@@ -13,11 +13,9 @@ int eventMessageHandler(char *message) {
     strncpy(tempMessage, message, strlen(message) + 1);
     event = strtok(tempMessage, ":");
     strncpy(payload, tempMessage + strlen(event) + 1, strlen(tempMessage) - strlen(event));
+
     if (strcmp(event, EXIT_SERVER) == 0)
         kill(getpid(), SIGINT);
     else if(strcmp(event, UPDATE_TEMP) == 0)
         handlerEnvData();
-    else {
-        printf("Error: Expected event type of EVENT_HANDLER\n");
-    }
 }
